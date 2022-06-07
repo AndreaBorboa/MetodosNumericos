411e6ed82a7a23d5e0419e8690ed2b7f35aed50c
Andrea Borboa Ramírez <andrea.borboar@potros.itson.edu.mx>
2022-06-07 16:05:44 -0700

    reglaFalsa ejercicio añadido.

---
+++ src/reglaFalsaEjercicio/ReglaFalsa.java
@@ -0,0 +1,93 @@
+
+package reglaFalsaEjercicio;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andrea Borboa Ramírez
+ */
+public class ReglaFalsa {
+
+    /**
+     * Programa que calcula la raiz de la funcion f(x) = x^3 - 7.014x^2 - 13.024x - 3.548
+     */
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+    
+    //Se solicitan los datos del valor de x izquierda, x derecha y el error. 
+    System.out.print("Valor de x izquierda: ");
+    double xizq = scanner.nextDouble();
+    System.out.print("Valor de x derecha: ");
+    double xder = scanner.nextDouble();
+    System.out.print("Error: ");
+    double error = scanner.nextDouble();
+    
+        reglaFalsa(xizq, xder, error);
+    
+    }
+    
+    private static void reglaFalsa(double xizq, double xder, double error){
+        DecimalFormat formato = new DecimalFormat("0.######");//formato para los decimales de resultados. 
+        
+        
+        double xm = 0;
+        double fxm=0;
+        
+        double fxder=f(xder);
+        double fxizq=f(xizq);
+        
+        xm = ((xizq*fxder)- (xder*(fxizq))) / (fxder - fxizq); // calcula xm
+        fxm=f(xm); //obtiene f(xm) llamando al metodo "f" y enviando como parametro xm
+        
+        
+        //Encabezado de la tabla para los resultados 
+        System.out.println("─♡──────────────────────────────────────♡─");
+        System.out.printf("%10s %10s %10s %10s %10s %10s", "xi", "xd", "f(xi)", "f(xd)","xm","f(xm)");
+        System.out.println();
+        System.out.println("─♡──────────────────────────────────────♡─");
+        
+        //Ciclo que se repite mientras el valor absoluto de f(xm) sea mayor al error. 
+        while(Math.abs(fxm)>error){
+            
+            fxizq = f(xizq); // se calcula f(xizq) llamando al metodo "f"
+            fxder = f(xder);// se calcula f(xder) llamando al metodo "f"
+            xm = ((xizq*fxder)- (xder*(fxizq))) / (fxder - fxizq); // calcula xm
+            fxm=f(xm);// calcula f(xm) llamando al metodo "f"
+            
+            //Se imprimen los resultados en la tabla con el formato indicado.
+            System.out.format("%10s %10s %10s %10s %10s %10s",
+                    formato.format(xizq),formato.format(xder),formato.format(fxizq),formato.format(fxder),formato.format(xm),formato.format(fxm));
+            System.out.println();
+            
+           
+            //condicion para reemplazar x izquiera o x derecha segun sea el caso. 
+            if(fxm<0){
+                if(fxizq<0){
+                    xizq=xm;
+                }else{
+                    xder=xm;
+                }
+               
+            }else{
+                if(fxizq>0){
+                    xizq=xm;
+                }else{
+                    xder=xm;
+                }
+                
+            } 
+        }
+        System.out.println("─♡──────────────────────────────────────♡─");
+        
+    }
+    
+     //Metodo que calcula la funcion solicitada 
+    private static double f(double x) {
+        return (Math.pow(x, 3) - (7.014 * Math.pow(x, 2)) - (13.024 * x) - 3.548);
+    }
+    
+}
+
+    
