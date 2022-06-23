4bca54dfe10eb9a482354c99d3a740cc12105b8d
Andrea Borboa Ramírez <andrea.borboar@potros.itson.edu.mx>
2022-06-22 20:33:53 -0700

    Metodo Gauss Seidel

--- src/gaussSeidel/GaussSeidel.java
+++ src/gaussSeidel/GaussSeidel.java
@@ -1,4 +1,3 @@
-
 package gaussSeidel;
 
 import java.text.DecimalFormat;
@@ -16,17 +15,17 @@ public class GaussSeidel {
     double[] xactuales = new double[3]; //array que almacena las x_i de la iteracion actual
     double[] xanteriores = new double[3]; // array que almacena las x_i de la iteracion anterior
     double[] abs = new double[3]; // array que almacena los valores absolutos de las x_i
-    
-    
-    
+
+
+
     //Programa que calcula los valores de x para solucionar el sistema de ecuaciones por el metodo de Gauss Seidel. 
     public static void main(String[] args) {
-        GaussSeidel seidel= new GaussSeidel();
+        GaussSeidel seidel = new GaussSeidel();
         seidel.leeEcuaciones();
         seidel.gaussSeidel();
     }
 
-    public void leeEcuaciones() {//metodo para leer las ecuaciones y el error
+    public void leeEcuaciones() { //metodo para leer las ecuaciones y el error
 
         Scanner tec = new Scanner(System.in);
 
@@ -48,93 +47,93 @@ public class GaussSeidel {
             System.out.println("Resultado de ecuación " + (f + 1) + ":"); // lee los "=" de cada ecuación. 
             this.sistemaEcuaciones[f][3] = tec.nextDouble();
         }
-        
+
         System.out.println("————————— Error ————————");
         error = tec.nextDouble(); //solicita el error aproximado. 
     }
 
     public void gaussSeidel() {
-        double x1=0, x2=0, x3=0; //inicializamos x1,x2 y x3 en 0.
-        abs[0]=1; //inicializamos los valores de valor absoluto para que pueda entrar al ciclo. 
-        abs[1]=1;
-        abs[2]=1;
-        
+        double x1 = 0, x2 = 0, x3 = 0; //inicializamos x1,x2 y x3 en 0.
+        abs[0] = 1; //inicializamos los valores de valor absoluto para que pueda entrar al ciclo. 
+        abs[1] = 1;
+        abs[2] = 1;
+
         System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
         System.out.println("⚘ — — — — — — — — — — | METODO GAUSS SEIDEL |— — — — — — — — — — ⚘");
         System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━");
         System.out.println("");
-        
 
-            
-        while ( (Math.abs(abs[0])) > error && (Math.abs(abs[1])) > error && (Math.abs(abs[2]))> error ) { //Se repite hasta que los valores absolutos de las x sean menores o iguales al error. 
-            
+
+
+        while ((Math.abs(abs[0])) > error && (Math.abs(abs[1])) > error && (Math.abs(abs[2])) > error) { //Se repite hasta que los valores absolutos de las x sean menores o iguales al error. 
+
             //Se calcula x1 y se almacena en el arreglo de las x actuales para utilizarla en x2 y x3.
-            x1 = this.sistemaEcuaciones[0][3] + ( ( (this.sistemaEcuaciones[0][1]) * (-1)) * (xanteriores[1])) + (  ( (this.sistemaEcuaciones[0][2]) * (-1) )  * (xanteriores[2]) ) ;
-            if (this.sistemaEcuaciones[0][0]<0){
-                x1 = (x1) / ( (this.sistemaEcuaciones[0][0])* -1);
-            }else{
+            x1 = this.sistemaEcuaciones[0][3] + (((this.sistemaEcuaciones[0][1]) * (-1)) * (xanteriores[1])) + (((this.sistemaEcuaciones[0][2]) * (-1)) * (xanteriores[2]));
+            if (this.sistemaEcuaciones[0][0] < 0) {
+                x1 = (x1) / ((this.sistemaEcuaciones[0][0]) * -1);
+            } else {
                 x1 = x1 / this.sistemaEcuaciones[0][0];
             }
             xactuales[0] = x1;
-           
+
             //Se calcula x2 y se almacena en el arreglo de las x actuales para utilizarla en x3. 
-            x2 = this.sistemaEcuaciones[1][3] + ( ( (this.sistemaEcuaciones[1][0]) * (-1)) * (xactuales[0])) + ( ( (this.sistemaEcuaciones[1][2]) * (-1)) * (xanteriores[2]) ) ;
-            if (this.sistemaEcuaciones[1][1]<0){
-                x2 = (x2) / ( (this.sistemaEcuaciones[1][1])* -1);
-            }else{
+            x2 = this.sistemaEcuaciones[1][3] + (((this.sistemaEcuaciones[1][0]) * (-1)) * (xactuales[0])) + (((this.sistemaEcuaciones[1][2]) * (-1)) * (xanteriores[2]));
+            if (this.sistemaEcuaciones[1][1] < 0) {
+                x2 = (x2) / ((this.sistemaEcuaciones[1][1]) * -1);
+            } else {
                 x2 = x2 / this.sistemaEcuaciones[1][1];
             }
-            xactuales[1]=x2;
-            
+            xactuales[1] = x2;
+
             //Se calcula x3 y se almacena en el arreglo de las x actuales para utilizarla en x1.  
-            x3 = this.sistemaEcuaciones[2][3] + ( ( (this.sistemaEcuaciones[2][0]) * (-1)) * (xactuales[0])) + ( ( (this.sistemaEcuaciones[2][1]) * (-1)) * (xactuales[1]) );
-            if (this.sistemaEcuaciones[2][2]<0){
-                x3 = (x3) / ( (this.sistemaEcuaciones[2][2])* -1);
-            }else{
+            x3 = this.sistemaEcuaciones[2][3] + (((this.sistemaEcuaciones[2][0]) * (-1)) * (xactuales[0])) + (((this.sistemaEcuaciones[2][1]) * (-1)) * (xactuales[1]));
+            if (this.sistemaEcuaciones[2][2] < 0) {
+                x3 = (x3) / ((this.sistemaEcuaciones[2][2]) * -1);
+            } else {
                 x3 = x3 / this.sistemaEcuaciones[2][2];
             }
-            xactuales[2]=x3;
-            
+            xactuales[2] = x3;
+
             //Se hacen las restas de la x actual - la x anterior y se almacenan en el arreglo de abs para ser utilizado en la impresion. 
             abs[0] = xactuales[0] - xanteriores[0];
             abs[1] = xactuales[1] - xanteriores[1];
             abs[2] = xactuales[2] - xanteriores[2];
-            
+
             //Ahora las x anteriores van a ser igual a las x actuales. 
             xanteriores[0] = xactuales[0];
             xanteriores[1] = xactuales[1];
             xanteriores[2] = xactuales[2];
-            
+
             despliegaSolucion(); //llamamos al metodo despliegaSolucion() para imrpimir las x actuales y los valores absolutos. 
-            
-            
+
+
             iteraciones++; //incrementamos el numero de iteraciones
         }
-        
+
     }
-    
-    public void despliegaSolucion(){ //metodo para imrpimir los valores de las x y valores absolutos de las x. 
+
+    public void despliegaSolucion() { //metodo para imrpimir los valores de las x y valores absolutos de las x. 
         DecimalFormat formato = new DecimalFormat("0.######"); //formato para la impresion. 
-        
-        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Iteración: "+iteraciones+" ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"); //imrpimimos el numero de la iteracion actual. 
-        
+
+        System.out.println("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Iteración: " + iteraciones + " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"); //imrpimimos el numero de la iteracion actual. 
+
         System.out.println("");
-        
+
         //ciclo para imprimir los valores de x
-        for(int i =0; i<xanteriores.length; i++){
-            System.out.println("x"+ (i+1) +": "+formato.format(xanteriores[i]));
+        for (int i = 0; i < xanteriores.length; i++) {
+            System.out.println("x" + (i + 1) + ": " + formato.format(xanteriores[i]));
         }
-        
+
         System.out.println("");
         //ciclo para imprimir los valores absolutos de las x. 
-        if(iteraciones>0){
-            for(int i =0; i<abs.length; i++){
-                System.out.println("Valor absoluto x"+ (i+1) +": "+formato.format(abs[i]));
-            }  
+        if (iteraciones > 0) {
+            for (int i = 0; i < abs.length; i++) {
+                System.out.println("Valor absoluto x" + (i + 1) + ": " + formato.format(abs[i]));
+            }
         }
-        
-        
-        
+
+
+
     }
 
 
